fb04282f48ca6fc582f004a1ffd188d737308b56
nuritnt <ttn@gmx.ch>
2017-02-10 15:14:52 +0100

    set up active listener bot

---
+++ listener.rb
@@ -0,0 +1,36 @@
+require 'slack-ruby-client'
+require 'eventmachine'
+require 'faye/websocket'
+require 'uri'
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :hello do
+  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
+end
+
+client.on :message do |data|
+  case data.text
+  when 'bot hi' then
+    client.message channel: data.channel, text: "Hi <@#{data.user}>!"
+  when /bot/ then
+    client.message channel: data.channel, text: "Sorry <@#{data.user}>, what?"
+  when /https?:\/\/[\S]+/ then
+    links = URI.extract(data.text)
+    client.message channel: data.channel, text: "You just posted a link."
+  end
+end
+
+client.on :close do |_data|
+  puts "Client is about to disconnect"
+end
+
+client.on :closed do |_data|
+  puts "Client has disconnected successfully!"
+end
+
+client.start!
