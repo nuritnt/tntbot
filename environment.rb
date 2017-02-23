1cfc49ba25abd24be2ef3309c532e4dab8835a02
nuritnt <ttn@gmx.ch>
2017-02-23 13:20:14 +0100

    adapt production development environment

--- environment.rb
+++ environment.rb
@@ -9,8 +9,21 @@ Dir.glob("./db/migrations/*.rb").each do |file|
   require file
 end
 
-# tells AR what db file to use
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection(
+            :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+            :host     => db.host,
+            :username => db.user,
+            :password => db.password,
+            :database => db.path[1..-1],
+            :encoding => 'utf8'
+    )
+end
+
+configure :development do
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
-  :database => ENV['DATABASE_URL']
+  :database => 'tntbot_production'
 )
+end
