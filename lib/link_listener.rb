777a09869a995edf45ccb4c0e569cf324e7bd137
nuritnt <ttn@gmx.ch>
2017-03-01 13:56:25 +0100

    fix url regex, add find or initialize method for database

--- lib/link_listener.rb
+++ lib/link_listener.rb
@@ -1,33 +1,33 @@
 require_relative "../environment.rb"
 require 'slack-ruby-bot'
-require 'slack-ruby-client'
 require 'curb'
-require 'eventmachine'
-require 'faye/websocket'
+require 'uri'
+require "pry"
 
 class Bot < SlackRubyBot::Bot
   command 'ping' do |client, data, _match|
     client.say(text: "pong'd from tnt's device mofo", channel: data.channel)
   end
 
-  match /(https?:\/\/(?:www\.|(?!www))[^\s\.]{4,}+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/ do |client, data, match|
-    client.say(channel: data.channel, text: "#{match[:link]} yay, you just posted an evilish link. - added to my sneaky database!")
-    Link.create(
-      url: match[:link],
-      channel_id: data.channel,
-      user_id: data.user
-    )
+  match(/(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix) do |client, data, match|
+    url = match.to_s
+    link = Link.find_or_initialize_by(url: url)
+    if link.new_record?
+      link.update_attributes(
+        title: URI.parse(url).host,
+        channel_id: data.channel,
+        user_id: data.user
+      )
+      link.save
+      client.say(channel: data.channel, text: "successfully saved to database.")
+    else
+      link.touch
+      client.say(channel: data.channel, text: "already in my database.")
+    end
   end
-
 end
 
 
 SlackRubyBot::Client.logger.level = Logger::WARN
 
 Bot.run
-#
-# when /https?:\/\/[\S]+/ then
-#   links = URI.extract(data.text)
-#   client.message channel: data.channel, text: "You just posted a link."
-# end
-# client.message channel: data.channel, text: "Sorry <@#{data.user}>, what?"
