6e5387578448ba5a00fad661b142fffd9e2165d9
nuritnt <ttn@gmx.ch>
2017-03-01 14:15:50 +0100

    update db table

--- db/migrations/001_create_links_table.rb
+++ db/migrations/001_create_links_table.rb
@@ -3,6 +3,7 @@ require_relative '../../environment'
 class CreateLinksTable < ActiveRecord::Migration
     def up
       create_table :links do |t|
+        t.string :title
         t.string :user_id
         t.string :channel_id
         t.string :url
