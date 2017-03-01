8716bdbcca63992dbc6d7a78b9fe104c7ed231fc
nuritnt <ttn@gmx.ch>
2017-03-01 16:14:26 +0100

    change title from domain to title from html

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sinatra'
 gem 'activerecord'
 gem 'rake'
 gem 'curb'
+gem 'nokogiri'
 
 group :development do
   gem 'pry'
--- lib/link_listener.rb
+++ lib/link_listener.rb
@@ -2,7 +2,9 @@ require_relative "../environment.rb"
 require 'slack-ruby-bot'
 require 'curb'
 require 'uri'
-require "pry"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 class Bot < SlackRubyBot::Bot
   command 'ping' do |client, data, _match|
@@ -12,9 +14,10 @@ class Bot < SlackRubyBot::Bot
   match(/(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix) do |client, data, match|
     url = match.to_s
     link = Link.find_or_initialize_by(url: url)
+    title = Nokogiri::HTML(open(url)).css('title').text
     if link.new_record?
       link.update_attributes(
-        title: URI.parse(url).host,
+        title: title,
         channel_id: data.channel,
         user_id: data.user
       )
